ed19df659d76715124f815a5e8a9cb1994ad2b27
Josh Gilstrap <jgilstrap23@gmail.com>
2023-01-15 10:10:55 -0800

    merge sort implemented

--- sorting_algs.py
+++ sorting_algs.py
@@ -1,13 +1,18 @@
-import timeit
-import random
+from timeit import repeat
+from random import randint
 
-ARR_SIZE = 100
+ARR_SIZE = 100000
+MAX_VALUE = 1000
+
+def time_algorithm(alg, arr):
+    setup = f'from __main__ import {alg}' \
+        if alg != 'sorted' else ''
+    stmt = f'{alg}({arr})'
+    times = repeat(setup=setup, stmt=stmt, repeat=3, number=10)
+    print(f'Algorithm: {alg}. Minimum execution time: {min(times)}')
 
 def make_arr():
-    arr = []
-    for num in range(ARR_SIZE):
-        num = random.randint(0,999)
-        arr.append(num)
+    arr = [randint(0,MAX_VALUE) for i in range(ARR_SIZE)]
     return arr
 
 
@@ -29,7 +34,47 @@ def insertion_sort(nums):
         nums[j + 1] = target
     return nums
 
+def merge_sort(nums):
+    if len(nums) < 2:
+        return nums
+
+    middle = len(nums) // 2
+
+    left = merge_sort(nums[:middle])
+    right = merge_sort(nums[middle:])
+    return merge(left, right)
+
+def merge(arr1, arr2):
+    if len(arr1) == 0:
+        return arr2
+    if len(arr2) == 0:
+        return arr1
+
+    merged_arr = []
+    place1 = 0
+    place2 = 0
+    while len(merged_arr) < (len(arr1) + len(arr2)):
+        if arr1[place1] <= arr2[place2]:
+            merged_arr.append(arr1[place1])
+            place1 += 1
+        else:
+            merged_arr.append(arr2[place2])
+            place2 += 1
+
+        if place1 == len(arr1):
+            merged_arr += arr2[place2:]
+            break
+        if place2 == len(arr2):
+            merged_arr += arr1[place1:]
+            break
+    return merged_arr
+
 n = make_arr()
-print(n)
-insertion_sort(n)
-print(n)
+print(f'Size: {len(n)}\n')
+print('|--------------------------------------------------------------------------|')
+# time_algorithm('bubble_sort', n)
+# print('\n')
+# time_algorithm('insertion_sort', n)
+# print('\n')
+# time_algorithm('merge_sort', n)
+print('|--------------------------------------------------------------------------|')
